634c3570d89dd63e3a639f721ec344f5e3ceeab9
backup <backup@backup-pc>
2020-11-18 13:31:23 +0530

    Eureka Server

---
+++ src/main/java/com/eureka/client/eureka/EurekaApplication.java
@@ -0,0 +1,23 @@
+package com.eureka.client.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController        
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+        
+        @RequestMapping(value = "/")
+        public String home() {
+                return "Eureka Client application";
+        }
+}
